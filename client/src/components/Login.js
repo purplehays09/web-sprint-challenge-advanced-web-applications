d64f991bd4e3a0d92473781b6c03a01d382e5d48
david hays <purplehays527@gmail.com>
2020-10-16 09:25:30 -0600

    I added the login form and such and it seems to be working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios'
+
+const initialForm = {
+  username:'',
+  password:''
+}
 
 const Login = () => {
+  const [formValues,setFormValues] = useState(initialForm)
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const handleChanges = e => {
+    const {name,value} = e.target
+
+    setFormValues({
+      ...formValues,
+      [name]:value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    axios.post('http://localhost:5000/api/login/',formValues)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => console.log(err))
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='username'
+          value={formValues.username}
+          placeholder='username'
+          onChange={handleChanges}
+        />
+
+        <input
+          type='password'
+          name='password'
+          value={formValues.password}
+          placeholder='password'
+          onChange={handleChanges}
+        />
+
+        <button>Submit</button>
+      </form>
     </>
   );
 };
