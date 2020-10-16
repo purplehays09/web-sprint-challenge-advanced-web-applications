a9e1c588f3176fd8618469cf47c79e252721a332
david hays <purplehays527@gmail.com>
2020-10-16 11:07:33 -0600

    working on testing now

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
-test("Fetches data and renders the bubbles", () => {
+jest.mock("../api/colors")
+
+test("Fetches data and renders the bubbles", async () => {
   // Finish this test
+  // Arrange
+
+
+  //Act
+
+
+  //Assert
 });
