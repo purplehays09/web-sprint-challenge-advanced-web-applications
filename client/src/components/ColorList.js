3de88ab5582374da7c018bbd82230db04ad016c5
david hays <purplehays527@gmail.com>
2020-10-16 10:22:01 -0600

    Added the saveEdit functionality

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth'
 
 const initialColor = {
   color: "",
@@ -7,7 +7,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  console.log("COLOR LIST COLORS ===>",colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -21,10 +21,20 @@ const ColorList = ({ colors, updateColors }) => {
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    console.log("colorToEdit ===> ", colorToEdit)
+
+    axiosWithAuth().put(`colors/${colorToEdit.id}`,colorToEdit)
+    .then(res => {
+      console.log(res.data)
+      updateColors(colors)
+      setEditing(false)
+    })
+    .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    
   };
 
   return (
